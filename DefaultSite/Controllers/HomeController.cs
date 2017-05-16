78684f576325342572bdb9d3b68bff030a5a9c91
Kent Pytlik <kpytlik@webershandwick.com>
2017-05-16 17:01:39 -0500

    Initial Commit

---
+++ DefaultSite/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DefaultSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Response.StatusCode = 404;
+            return null;
+        }
+    }
+}
---
+++ DefaultSite/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace DefaultSite
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+
+        protected void Application_EndRequest()
+        {
+            // removing excessive headers. They don't need to see this.
+            Response.Headers.Remove("Server");
+            Response.Headers.Remove("X-AspNet-Version");
+            Response.Headers.Remove("X-AspNetMvc-Version");
+            Response.Headers.Remove("X-Powered-By");
+            Response.Headers.Remove("X-SourceFiles");
+        }
+    }
+}
